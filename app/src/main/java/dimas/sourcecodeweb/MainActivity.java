875cbac79aa90825d3b82a0efb9a33e103e814e6
dimasrs <ragil825@gmail.com>
2017-10-21 12:46:04 +0700

    get_source_code

--- app/src/main/java/dimas/sourcecodeweb/MainActivity.java
+++ app/src/main/java/dimas/sourcecodeweb/MainActivity.java
@@ -1,12 +1,15 @@
 package dimas.sourcecodeweb;
 
+import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,21 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
     ConnectivityManager myConnManager;
     NetworkInfo myInfo;
+    private Spinner sp;
+    EditText ed;
+    String edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myText = (TextView)findViewById(R.id.myResult);
+        sp = (Spinner)findViewById(R.id.spinner);
+        ed = (EditText)findViewById(R.id.editText);
 
         myConnManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
         myInfo = myConnManager.getActiveNetworkInfo();
     }
+    public boolean isOnline(){
+        ConnectivityManager cm =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo myInfo = cm.getActiveNetworkInfo();
+        if(myInfo != null && myInfo.isConnectedOrConnecting()){
+            return true;
+        }
+        return false;
+    }
 
     public void doSomething(View view) {
-        if(myInfo != null && myInfo.isConnected()) {
+        if(isOnline()== true) {
             c1 = new ConnectInternetTask(this);
-            c1.execute("http://www.google.com");
+            c1.execute(sp.getSelectedItem()+ ed.getText().toString());
         }
         else {
             Toast.makeText(this,"Internet Not Connected",Toast.LENGTH_SHORT).show();
